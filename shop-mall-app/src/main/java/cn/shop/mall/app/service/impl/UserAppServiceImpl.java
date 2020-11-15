a0e4efecfc59beb321fa707f00f372f576771cdf
571120205@qq.com <571120205@qq.com>
2020-11-15 21:33:31 +0800

    提交配置

---
+++ shop-mall-app/src/main/java/cn/shop/mall/app/service/impl/UserAppServiceImpl.java
@@ -0,0 +1,16 @@
+package cn.shop.mall.app.service.impl;
+
+import cn.shop.mall.app.service.UserAppService;
+import cn.shop.mall.center.entity.UserAppEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by LSY on 2020/11/15.
+ */
+@Service
+public class UserAppServiceImpl implements UserAppService{
+    @Override
+    public UserAppEntity getById(Long userId) {
+        return null;
+    }
+}
